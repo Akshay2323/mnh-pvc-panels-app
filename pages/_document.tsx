1778a9090b4d013067f44f40c3145746084ee099
Shilpesh Patel <68633303+SKTechnotion@users.noreply.github.com>
2025-08-21 22:18:20 +0530

    menu fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,89 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/media/icons/iconsmind/line-icons.min.css" />
       </Head>
-      <body className="antialiased">
+      <body className="page-main">
+        <div id="preloader"></div>
+        <nav className="menu-classic menu-fixed menu-transparent light align-right" data-menu-anima="fade-in">
+          <div className="container">
+            <div className="menu-brand">
+              <a href="#">
+                <img className="logo-default scroll-hide" src="assets/logo-white-blue.svg" alt="logo" />
+                <img className="logo-retina scroll-hide" src="assets/logo-white-blue.svg" alt="logo" />
+                <img className="logo-default scroll-show" src="assets/logo-white-solid.svg" alt="logo" />
+                <img className="logo-retina scroll-show" src="assets/logo-white-solid.svg" alt="logo" />
+              </a>
+            </div>
+            <i className="menu-btn"></i>
+            <div className="menu-cnt">
+              <ul id="main-menu">
+                <li className="dropdown">
+                  <a href="#">Home</a>
+                  <ul>
+                    <li><a href="index.html">Main</a></li>
+                    <li><a href="index-2.html">Home two</a></li>
+                    <li><a href="index-3.html">Home three</a></li>
+                  </ul>
+                </li>
+                <li className="dropdown">
+                  <a href="#">Pages</a>
+                  <ul>
+                    <li className="dropdown-submenu">
+                      <a>About</a>
+                      <ul>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="team.html">Team</a></li>
+                        <li><a href="team-2.html">Team two</a></li>
+                      </ul>
+                    </li>
+                    <li className="dropdown-submenu">
+                      <a>Special</a>
+                      <ul>
+                        <li><a href="food.html">Food</a></li>
+                        <li><a href="shelters.html">Shelters</a></li>
+                        <li><a href="events.html">Events</a></li>
+                      </ul>
+                    </li>
+                    <li className="dropdown-submenu">
+                      <a>Others</a>
+                      <ul>
+                        <li><a href="prices.html">Prices</a></li>
+                        <li><a href="history.html">History</a></li>
+                        <li><a href="gallery.html">Gallery</a></li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="elements/components/buttons.html">Elements</a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="dropdown">
+                  <a href="treks.html">Treks</a>
+                  <ul>
+                    <li><a href="treks.html">Treks</a></li>
+                    <li><a href="treks-single.html">Single trek</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="blog.html">Blog</a>
+                </li>
+                <li>
+                  <a href="contacts.html">Contacts</a>
+                </li>
+              </ul>
+              <div className="menu-right">
+                <ul className="lan-menu">
+                  <li className="dropdown">
+                    <a href="#"><img src="assets/en.png" />EN </a>
+                    <ul>
+                      <li><a href="#"><img src="assets/it.png" />IT</a></li>
+                      <li><a href="#"><img src="assets/es.png" />ES</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
         <Main />
         <NextScript />
       </body>
