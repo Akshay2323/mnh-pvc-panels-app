fc95d7d6b995cbbd3b464b1c0283eb8eadec93d0
Axay Patel <54527353+Akshay2323@users.noreply.github.com>
2025-09-01 16:50:10 +0530

    add blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,216 @@
+export default function Blog() {
+    return (
+        <div><header className="header-image ken-burn-center light" data-parallax="true" data-natural-height="1080" data-natural-width="1920" data-bleed="0" data-image-src="http://via.placeholder.com/1920x1080" data-offset="0">
+        <div className="container">
+            <h1>The blog</h1>
+            <h2>Enjoy a day on the top dont have a price</h2>
+            <ol className="breadcrumb">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#">Pages</a></li>
+                <li><a href="#">Blog</a></li>
+            </ol>
+        </div>
+    </header>
+    <main>
+        <section className="section-base section-color">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8">
+                        <div className="grid-list" data-columns="1">
+                            <div className="grid-box">
+                                <div className="grid-item">
+                                    <div className="cnt-box cnt-box-blog-side boxed" data-href="post.html">
+                                        <a href="post.html" className="img-box">
+                                            <div className="blog-date">
+                                                <span>01</span>
+                                                <span>JAN 2020</span>
+                                            </div>
+                                            <img src="http://via.placeholder.com/800x500" alt="" />
+                                        </a>
+                                        <div className="caption">
+                                            <h2>Content box</h2>
+                                            <ul className="icon-list icon-list-horizontal">
+                                                <li><i className="icon-calendar"></i><a href="#">01 / 15 / 2020</a></li>
+                                                <li><i className="icon-bookmark"></i><a href="#">Treks</a></li>
+                                                <li><i className="icon-user"></i><a href="#">Admin</a></li>
+                                            </ul>
+                                            <p>
+                                                Lorem ipsum dolor sitamet consectetur adipisicing elito sed do eiusmod tempore.amet consectetur adipisicing elito sed do eiusmod tempore.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="grid-item">
+                                    <div className="cnt-box cnt-box-blog-side boxed" data-href="post.html">
+                                        <a href="post.html" className="img-box">
+                                            <div className="blog-date">
+                                                <span>09</span>
+                                                <span>JAN 2020</span>
+                                            </div>
+                                            <img src="http://via.placeholder.com/800x500" alt="" />
+                                        </a>
+                                        <div className="caption">
+                                            <h2>Content box</h2>
+                                            <ul className="icon-list icon-list-horizontal">
+                                                <li><i className="icon-calendar"></i><a href="#">01 / 15 / 2020</a></li>
+                                                <li><i className="icon-bookmark"></i><a href="#">Treks</a></li>
+                                                <li><i className="icon-user"></i><a href="#">Admin</a></li>
+                                            </ul>
+                                            <p>
+                                                Lorem ipsum dolor sitamet consectetur adipisicing elito sed do eiusmod tempore.amet consectetur adipisicing elito sed do eiusmod tempore.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="grid-item">
+                                    <div className="cnt-box cnt-box-blog-side boxed" data-href="post.html">
+                                        <a href="post.html" className="img-box">
+                                            <div className="blog-date">
+                                                <span>20</span>
+                                                <span>JAN 2020</span>
+                                            </div>
+                                            <img src="http://via.placeholder.com/800x500" alt="" />
+                                        </a>
+                                        <div className="caption">
+                                            <h2>Content box</h2>
+                                            <ul className="icon-list icon-list-horizontal">
+                                                <li><i className="icon-calendar"></i><a href="#">01 / 15 / 2020</a></li>
+                                                <li><i className="icon-bookmark"></i><a href="#">Treks</a></li>
+                                                <li><i className="icon-user"></i><a href="#">Admin</a></li>
+                                            </ul>
+                                            <p>
+                                                Lorem ipsum dolor sitamet consectetur adipisicing elito sed do eiusmod tempore.amet consectetur adipisicing elito sed do eiusmod tempore.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="grid-item">
+                                    <div className="cnt-box cnt-box-blog-side boxed" data-href="post.html">
+                                        <a href="post.html" className="img-box">
+                                            <div className="blog-date">
+                                                <span>10</span>
+                                                <span>FEB 2020</span>
+                                            </div>
+                                            <img src="http://via.placeholder.com/800x500" alt="" />
+                                        </a>
+                                        <div className="caption">
+                                            <h2>Content box</h2>
+                                            <ul className="icon-list icon-list-horizontal">
+                                                <li><i className="icon-calendar"></i><a href="#">01 / 15 / 2020</a></li>
+                                                <li><i className="icon-bookmark"></i><a href="#">Treks</a></li>
+                                                <li><i className="icon-user"></i><a href="#">Admin</a></li>
+                                            </ul>
+                                            <p>
+                                                Lorem ipsum dolor sitamet consectetur adipisicing elito sed do eiusmod tempore.amet consectetur adipisicing elito sed do eiusmod tempore.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="grid-item">
+                                    <div className="cnt-box cnt-box-blog-side boxed" data-href="post.html">
+                                        <a href="post.html" className="img-box">
+                                            <div className="blog-date">
+                                                <span>25</span>
+                                                <span>FEB 2020</span>
+                                            </div>
+                                            <img src="http://via.placeholder.com/800x500" alt="" />
+                                        </a>
+                                        <div className="caption">
+                                            <h2>Content box</h2>
+                                            <ul className="icon-list icon-list-horizontal">
+                                                <li><i className="icon-calendar"></i><a href="#">01 / 15 / 2020</a></li>
+                                                <li><i className="icon-bookmark"></i><a href="#">Treks</a></li>
+                                                <li><i className="icon-user"></i><a href="#">Admin</a></li>
+                                            </ul>
+                                            <p>
+                                                Lorem ipsum dolor sitamet consectetur adipisicing elito sed do eiusmod tempore.amet consectetur adipisicing elito sed do eiusmod tempore.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="list-pagination">
+                                <ul className="pagination" data-page-items="4" data-options="scrollTop:true"></ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <form className="form-box">
+                            <div className="input-text-btn">
+                                <input className="input-text" type="text" placeholder="Search ..." /><input type="submit" value="Search" className="btn" />
+                            </div>
+                        </form>
+                        <hr className="space-sm" />
+                        <h3>Categories</h3>
+                        <hr className="space-xs" />
+                        <div className="menu-inner menu-inner-vertical">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Treks and paths
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Travels
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        People and kids
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Animals and nature
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <hr className="space-sm" />
+                        <h3>Latest posts</h3>
+                        <hr className="space-sm" />
+                        <div className="menu-inner menu-inner-vertical menu-inner-image">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <img src="http://via.placeholder.com/450x450" alt="" />
+                                        <span>February 12, 2020</span>
+                                        The last summer days
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="http://via.placeholder.com/450x450" alt="" />
+                                        <span>February 25, 2020</span>
+                                        Travels
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="http://via.placeholder.com/450x450" alt="" />
+                                        <span>April 19, 2020</span>
+                                        People and kids
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="http://via.placeholder.com/450x450" alt="" />
+                                        <span>April 20, 2020</span>
+                                        Animals and nature
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <hr className="space-sm" />
+                        <h3>Twitter</h3>
+                        <hr className="space-sm" />
+                        <div className="social-feed social-feed-tw social-slider" data-social-id="twitter" data-options="count:8,arrows:true,nav:true,controls:out,type:carousel,perView:1"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main></div>
+    )
+}
