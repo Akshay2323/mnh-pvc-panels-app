27dcd8ffdafc2a706c1a5c10ca309eefaf3ef10b
Shilpesh Patel <68633303+SKTechnotion@users.noreply.github.com>
2025-09-29 22:09:04 +0530

    add description

--- pages/subcategory/[slug].tsx
+++ pages/subcategory/[slug].tsx
@@ -35,7 +35,7 @@ export default function SubCategoryPage({ initialData }: SubCategoryPageProps) {
             />
             <PageHeader
                 title={initialData?.category?.name || ''}
-                // description={`High-quality ${initialData?.category?.name} for your home and office.`}
+                description={`High-quality ${initialData?.category?.name} for your home and office.`}
                 breadcrumbs={[
                     { label: initialData?.category?.name || 'Product', href: '' }
                 ]}
