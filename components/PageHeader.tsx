5a0b4e271f202e2387ef4cce242effdf49767c05
Shilpesh Patel <68633303+SKTechnotion@users.noreply.github.com>
2025-10-09 23:30:10 +0530

    fix responsive issue

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/header.module.css';
 
 interface PageHeaderProps {
@@ -12,6 +12,17 @@ interface PageHeaderProps {
 }
 
 const PageHeader: React.FC<PageHeaderProps> = ({ title, description, breadcrumbs = [] }) => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkMobile = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        checkMobile();
+        window.addEventListener('resize', checkMobile);
+        return () => window.removeEventListener('resize', checkMobile);
+    }, []);
     return (
         <header className={styles.headerBase}>
             <div className={styles.container}>
@@ -22,17 +33,20 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, description, breadcrumbs
                     {/* {description.length > 40 ? `${description.substring(0, 40)}...` : description} */}
                 </h2>
                 {/* )} */}
-                <ol className={styles.breadcrumb}>
-                    <li><Link href="/">Home</Link></li>
-                    {breadcrumbs.map((crumb, index) => (
-                        <li key={index}>
-                            {
-                                crumb.href ? <Link href={crumb.href}>{crumb.label}</Link> : <Link href={"#"}><span>{crumb.label}</span></Link>
-                            }
-                        </li>
-                    ))}
-                    {/* <li className={styles.active}>{title}</li> */}
-                </ol>
+                {
+                    !isMobile &&
+                    <ol className={styles.breadcrumb}>
+                        <li><Link href="/">Home</Link></li>
+                        {breadcrumbs.map((crumb, index) => (
+                            <li key={index}>
+                                {
+                                    crumb.href ? <Link href={crumb.href}>{crumb.label}</Link> : <Link href={"#"}><span>{crumb.label}</span></Link>
+                                }
+                            </li>
+                        ))}
+                        {/* <li className={styles.active}>{title}</li> */}
+                    </ol>
+                }
             </div>
         </header>
     );
